2e115d1b21e4c0816525dbf762e1abc057f4c843
d0p3t <d0p3t@users.noreply.github.com>
2019-04-04 22:14:06 +0200

    Add __resource.lua to repo instead of remote

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+name 'MailDelivery'
+description 'Standalone mail delivery job for Paleto Bay, Grapeseed and Sandy Shores.'
+author 'Remco Troost (https://www.d0p3t.nu)'
+version 'v1.0.2'
+url 'https://github.com/d0p3t/MailDelivery'
+
+client_script "MailDelivery.Client.net.dll"
